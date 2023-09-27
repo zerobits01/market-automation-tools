be6035fe7846e8e3cf507f6856802558daffb2ce
zerobits01 <zerobits01@yahoo.com>
2023-09-27 17:13:07 +0000

    solving some errors

--- tel_bot/tbot.py
+++ tel_bot/tbot.py
@@ -14,11 +14,6 @@ TBOT_CRED = os.environ['tbot_cred']
 # these are for saving interactive input state
 USERNAME, PASSWORD = range(2)
 
-def check_cred(username, password):
-    input_cred = username+":"+hashlib.sha256(
-        password.encode()
-    ).hexdigest()
-    return input_cred==TBOT_CRED
 
 # Command handlers
 
@@ -32,7 +27,7 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Available commands:\n"
                               "/start - Start the bot\n"
                               "/help - Show this help message\n"
-                              "/update-site - Update the site (requires username and password)")
+                              "/updatesite - Update the site (requires username and password)")
 
 
 def update_site_start(update: Update, context: CallbackContext) -> int:
@@ -50,22 +45,34 @@ def receive_password(update: Update, context: CallbackContext) -> int:
     username = context.user_data['username']
     password = update.message.text
     
-    
-    if check_cred(username, password):
-        # be sure that its executable
-        output = subprocess.run(
-            ["/home/zerobits01/tel_bot/update.sh"], 
-            shell=True
-        )
-        if output.returncode == 0:
-            update.message.reply_text(f"Site updated for username: {username}")
+    print(f"checking username and password {username}:{password}")
+    try:
+        input_cred = hashlib.sha256(
+            (username+":"+password).encode()
+        ).hexdigest()
+        print(input_cred, TBOT_CRED)
+
+        if input_cred==TBOT_CRED:
+            # be sure that its executable
+            print(f"start update process")
+            update.message.reply_text("starting udpate process, please waite...")
+            output = subprocess.run(
+                ["/home/zerobits01/market-automation-tools/tel_bot/utils/update.sh"], 
+                shell=True
+            )
+            print(output)
+            update.message.reply_text(f"output result: {output}")
+            if output.returncode == 0:
+                update.message.reply_text(f"Site updated for username: {username}")
+            else:
+                update.message.reply_text(f"Site couldnt be updated please contact @zerobits01")
+            print("done")
         else:
-            update.message.reply_text(f"Site couldnt be updated please contact @zerobits01")
+            update.message.reply_text(f"username or password is wrong")
             
-    else:
-        update.message.reply_text(f"username or password is wrong")
-        
-    return ConversationHandler.END
+        return ConversationHandler.END
+    except Exception as e:
+        print(e)
 
 
 def cancel(update: Update, context: CallbackContext) -> int:
@@ -81,7 +88,7 @@ def main():
     
     # how to define interactive inputs to get the input from user
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('update-site', update_site_start)],
+        entry_points=[CommandHandler('updatesite', update_site_start)],
         states={
             USERNAME: [MessageHandler(Filters.text & ~Filters.command, receive_username)],
             PASSWORD: [MessageHandler(Filters.text & ~Filters.command, receive_password)]
--- tel_bot/utils/update.sh
+++ tel_bot/utils/update.sh
@@ -4,7 +4,9 @@ cd /home/zerobits01/time-clone;
 
 if [ $? ]
 then
-        git pull && npm install && sudo systemctl restart batime.service
+	[[ $(cd /home/zerobits01/time-clone; git pull)=="Already up to date." ]] || do_changes=0 
+        [[ $do_changes -eq 0 ]] && npm install && npm run build && sudo systemctl restart kit365.service
+	echo `date` site update do_changes is '$do_changes' > /tmp/update.log
         if [ $? ]
         then
                 exit 0
