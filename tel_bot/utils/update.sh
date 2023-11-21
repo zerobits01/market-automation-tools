e066fc23bc7800f5d14266e236c8352f6c292ee7
zerobits01 <zerobits0101@gmail.com>
2023-11-21 16:27:11 +0330

    service name changed

--- tel_bot/utils/update.sh
+++ tel_bot/utils/update.sh
@@ -5,7 +5,7 @@ cd /home/zerobits01/time-clone;
 if [ $? ]
 then
 	do_changes=1; echo date; date > /tmp/update_changes.log; [[ $(cd /home/zerobits01/time-clone; git pull)=="Already up to date." ]] || do_changes=0 
-        [[ $do_changes -eq 0 ]] && echo install; npm install >> /tmp/update_changes.log && echo build; npm run build >> /tmp/update_changes.log && echo restart; sudo systemctl restart kit365.service >> /tmp/update_changes.log
+        [[ $do_changes -eq 0 ]] && echo install; npm install >> /tmp/update_changes.log && echo build; npm run build >> /tmp/update_changes.log && echo restart; sudo systemctl restart kit365-ui.service >> /tmp/update_changes.log
 	echo `date` site update do_changes is $do_changes >> /tmp/update.log
         if [ $? ]
         then
